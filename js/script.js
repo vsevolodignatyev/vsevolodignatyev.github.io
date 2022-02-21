71ec352e7ddad06bf79aa75075cadacc9592f503
vsevolodignatyev <ignatyev.vsevolod@gmail.com>
2022-02-21 06:07:37 +0300

    project structure

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var canvas = document.getElementById('canvas');
+
+    canvas.setAttribute('width', width);
+    canvas.setAttribute('height', height);
+
+}
