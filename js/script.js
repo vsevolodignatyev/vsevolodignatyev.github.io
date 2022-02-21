d468416b4af06417760911aafab2b67d9af415b4
vsevolodignatyev <ignatyev.vsevolod@gmail.com>
2022-02-21 07:29:22 +0300

    add mesh

--- js/script.js
+++ js/script.js
@@ -14,9 +14,14 @@ window.onload = function() {
     const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000);
     camera.position.set(0, 0, 1000);
 
-    renderer.render(scene, camera);
-
     var light = new THREE.AmbientLight(0xffffff);
     scene.add(light);
+
+    var geometry = new THREE.PlaneGeometry(300, 300, 12, 12);
+    var material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
+    var mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer.render(scene, camera);
 
 }
