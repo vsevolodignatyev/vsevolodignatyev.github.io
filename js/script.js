46fa92dd242886af7d840e5d726e7e9040cb6577
vsevolodignatyev <ignatyev.vsevolod@gmail.com>
2022-02-24 01:45:37 +0300

    sphere

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 window.onload = function() {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    var canvas = document.getElementById('canvas');
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let canvas = document.getElementById('canvas');
 
     canvas.setAttribute('width', width);
     canvas.setAttribute('height', height);
 
-    var renderer = new THREE.WebGLRenderer({canvas: canvas});
+    let renderer = new THREE.WebGLRenderer({canvas: canvas});
     renderer.setClearColor(0x000000);
 
-    const scene = new THREE.Scene();
+    let scene = new THREE.Scene();
 
-    const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000);
+    let camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000);
     camera.position.set(0, 0, 1000);
 
-    var light = new THREE.AmbientLight(0xffffff);
+    let light = new THREE.AmbientLight(0xffffff);
     scene.add(light);
 
-    var geometry = new THREE.PlaneGeometry(300, 300, 12, 12);
-    var material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
-    var mesh = new THREE.Mesh(geometry, material);
+    let geometry = new THREE.SphereGeometry (200, 12, 12);
+    let material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
+    let mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
     renderer.render(scene, camera);
